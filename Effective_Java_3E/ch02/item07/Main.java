b8cb3ed48a1aac9c761810649e946ca27b9bdbe4
yim3370 <71271289+yim3370@users.noreply.github.com>
2023-01-13 23:11:27 +0900

    Item7 메모리 테스트

---
+++ Effective_Java_3E/ch02/item07/Main.java
@@ -0,0 +1,10 @@
+package ch02.item07;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        for (int i = 0; i < 34; i++) {
+            stack.push(i);
+        }
+    }
+}
