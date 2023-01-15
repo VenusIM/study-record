fd6ae5567055015408377019fa34a894c3d37160
yim3370 <71271289+yim3370@users.noreply.github.com>
2023-01-15 23:51:25 +0900

    Item7 메모리 누수 테스트 2차

--- Effective_Java_3E/ch02/item07/Main.java
+++ Effective_Java_3E/ch02/item07/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
         for (int i = 0; i < 1000000000; i++) {
-            stack.push(i);
-//            stack.pop(); // 391285792
-            stack.popThenNull(); // 338888016
+            stack.push(new Integer(i));
+            stack.pop(); // 238615984
+//            stack.popThenNull(); // 298644320
         }
         System.out.println(" Free Memory >> "+ (Runtime.getRuntime().freeMemory()));
     }
